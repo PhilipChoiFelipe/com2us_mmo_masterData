92c3c37624f3e188280ee706f642b084bd528fae
diydriller <dhrhd080@naver.com>
2021-08-03 17:01:10 +0900

    all sheet read

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,12 +33,13 @@
             this.button2 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize) (this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(37, 40);
+            this.button1.Location = new System.Drawing.Point(37, 21);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(259, 40);
             this.button1.TabIndex = 0;
@@ -48,7 +49,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(649, 40);
+            this.button2.Location = new System.Drawing.Point(649, 21);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(105, 40);
             this.button2.TabIndex = 1;
@@ -67,7 +68,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(361, 40);
+            this.button3.Location = new System.Drawing.Point(357, 21);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(220, 40);
             this.button3.TabIndex = 3;
@@ -75,11 +76,21 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(95, 67);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(135, 26);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button2);
@@ -89,6 +100,8 @@
             ((System.ComponentModel.ISupportInitialize) (this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.ComboBox comboBox1;
 
         private System.Windows.Forms.Button button3;
 
--- Form1.cs
+++ Form1.cs
@@ -1,40 +1,77 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Windows.Forms;
 using System.Data.OleDb;
+using System.Runtime.CompilerServices;
+using Microsoft.VisualBasic.Logging;
 
 
 namespace com2us_mmo_masterData
 {
     public partial class Form1 : Form
     {
-        public static DataTable dtExcel;
+        private OleDbConnection con = null;
+        public Dictionary<String, DataTable> dictExcel = new Dictionary<string, DataTable>();
         public Form1()
         {
             InitializeComponent();
         }
-        
-        public DataTable ReadExcel(string fileName, string fileExt) {  
-            string conn = string.Empty;  
-            DataTable dtexcel = new DataTable();  
+
+        public void getConnection(string fileName, string fileExt)
+        {
+            string conn = string.Empty;
             if (fileExt.CompareTo(".xls") == 0) 
                 conn = @"provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + fileName + ";Extended Properties='Excel 8.0;HRD=Yes;IMEX=1';";   
             else  
-                conn = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties='Excel 12.0;HDR=NO';"; 
-            using(OleDbConnection con = new OleDbConnection(conn)) {
-                try
-                {
-                    OleDbDataAdapter oleAdpt = new OleDbDataAdapter("select * from [CharacterBaseData$]", con);
-                    oleAdpt.Fill(dtexcel);
-                }
-                catch
-                {
-                    MessageBox.Show("CharacterBaseData Sheet Not Found.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }  
-            }  
-            return dtexcel;  
+                conn = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties='Excel 12.0;HDR=NO';";
+
+            con = new OleDbConnection(conn);
+        }
+
+
+        
+        public DataTable ReadSheet(String sheetName) 
+        {
+            if (sheetName.LastIndexOf('$') == sheetName.Length - 1)
+                sheetName = sheetName.Substring(0, sheetName.Length - 1);
+            con.Open();
+            DataTable dtexcel = new DataTable();
+    
+            OleDbDataAdapter oleAdpt = new OleDbDataAdapter("select * from [" + sheetName + "$]", con);
+            oleAdpt.Fill(dtexcel);
+            con.Close();
+            return dtexcel;
         }  
+        
+        public String[] GetSheetName()
+        {
+            DataTable infoTable = null;
+            
+            con.Open();
+            infoTable = con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+            String[] sheet = new String[infoTable.Rows.Count];
+            int idx = 0;
+            foreach (DataRow row in infoTable.Rows)
+            {
+                String sheetName = row["TABLE_NAME"].ToString();
+                sheet[idx++]=sheetName;
+            }
+            con.Close();
+            
+            return sheet;
+        }
+
+        void ReadData(String[] sheetNameList)
+        {
+            for (int i = 0; i < sheetNameList.Length; i++)
+            {
+                String k = sheetNameList[i];
+                DataTable v = ReadSheet(k);
+                dictExcel.Add(k,v);
+            }
+        }
 
 
         private void button1_Click(object sender, EventArgs e)
@@ -47,15 +84,30 @@ namespace com2us_mmo_masterData
                 filePath = file.FileName;  
                 fileExt = Path.GetExtension(filePath); 
                 if (fileExt.CompareTo(".xls") == 0 || fileExt.CompareTo(".xlsx") == 0) {  
-                    try {  
+                    try
+                    {
+                        getConnection(filePath, fileExt);
+                        
                         dtExcel = new DataTable();  
-                        dtExcel = ReadExcel(filePath, fileExt); 
-                        dataGridView1.Visible = true;  
-                        dataGridView1.DataSource = dtExcel;  
-                    } catch (Exception ex) {  
+                        String[] sheetNameList=GetSheetName();
+                        ReadData(sheetNameList);
+                        comboBox1.Items.AddRange(sheetNameList);
+                        
+                        if (sheetNameList.Length >= 1)
+                        {
+                            dtExcel = ReadSheet(sheetNameList[0]);
+                            comboBox1.SelectedIndex = 0;
+                            dataGridView1.Visible = true;
+                            dataGridView1.DataSource = dtExcel;
+                        }
+                 
+                    } 
+                    catch (Exception ex) {  
                         MessageBox.Show(ex.Message.ToString());  
                     }  
-                } else {  
+                }
+                else 
+                {  
                     MessageBox.Show("Please choose .xls or .xlsx file only.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error); 
                 }  
             }  
@@ -68,6 +120,13 @@ namespace com2us_mmo_masterData
         private void button3_Click(object sender, EventArgs e)
         {
             
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataTable sheet = new DataTable();
+            sheet = ReadSheet(comboBox1.Text.Trim());
+            dataGridView1.DataSource=sheet;
         }
     }
 }
